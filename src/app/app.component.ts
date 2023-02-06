500e8273ce4bf45f5e6e798c891cd859b73a6520
cnrsmst <cnr.smst@hotmail.com>
2023-02-06 08:22:46 +0300

    iki

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+export class Heroes {
+
+  constructor(
+
+    public id:number,
+    public name:string,
+    public surnmae:string,
+    public nickname:string
+
+
+  ) {
+    
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularProje';
+
+
+export class AppComponent implements OnInit{
+
+  heroes: Heroes[] | undefined;
+  constructor(
+    private httpClient : HttpClient
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(){
+
+    this.httpClient.get<any>("http://localhost:3000/heroes").subscribe(
+
+
+    response => {
+      console.log(response);
+      this.heroes = response;
+    }
+    );
+  }
 }
